9a04f9e821813aac6c23eb72a8655a96bd6fa768
AMILCAR OFICIAL <amilkarurquiagaramos1@gmail.com>
2026-07-12 09:42:58 -0500

    Actualizar general-api.js

--- plugins/general-api.js
+++ plugins/general-api.js
@@ -36,7 +36,7 @@ function bold(texto) {
 
 export default {
   command: ["apistatus", "estadoapi", "apiinfo"],
-  category: "Info",
+  category: "General",
   description: "Muestra el estado de la API",
   run: async (sock, msg, args, context) => {
     const { chatId } = context;
